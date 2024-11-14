287c7e2d7584ddf45e90575dd0dcc3895f9956da
Jan Eberwein <S2410629003@fhooe.at>
2024-11-14 13:21:39 +0100

    Adding Next JS Layout and Folder Structure for different pages

---
+++ client/app/account/edit/page.tsx
@@ -0,0 +1,9 @@
+const AccountEditPage = () => {
+  return (
+    <div>
+      <h1>Edit Account</h1>
+    </div>
+  );
+};
+
+export default AccountEditPage;
---
+++ client/app/account/settings/page.tsx
@@ -0,0 +1,9 @@
+const AccountSettingsPage = () => {
+  return (
+    <div>
+      <h1>Account Settings</h1>
+    </div>
+  );
+};
+
+export default AccountSettingsPage;
