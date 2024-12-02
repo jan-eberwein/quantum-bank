42316acd070a8ec7b6f379d9b7fa0a47290f20f8
Jan Eberwein <S2410629003@fhooe.at>
2024-12-02 11:58:52 +0100

    Copilot Kit Popup

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,8 @@ import Sidebar from "../components/Layout/Sidebar";
 import "../styles/globals.css"; // Import global styles
 import { UserProvider } from "../components/Layout/UserContext";
 import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotPopup } from "@copilotkit/react-ui";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -15,6 +17,15 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
             <main className="flex-1 p-4">
               <CopilotKit runtimeUrl="/api/copilotkit">
                 {children}
+                <CopilotPopup
+                  instructions={
+                    "You are assisting the user as best as you can. Answer in the best way possible given the data you have."
+                  }
+                  labels={{
+                    title: "Popup Assistant",
+                    initial: "Need any help?",
+                  }}
+                />
               </CopilotKit>
             </main>
           </div>
